7fe51b908afabe7c0c0c73307b96833c1b68a0ef
Rodolfo Cordeiro <270757@unifio.edu.br>
2025-08-13 16:54:46 -0300

    começando o curso

---
+++ src/test/java/com/rodolfo/controller/UserController.java
@@ -0,0 +1,14 @@
+package com.rodolfo.controller;
+
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+
+@Path("/users")
+public class UserController {
+
+    @GET
+    public String hello() {
+        return "Hello from UserController";
+    }
+}
+
